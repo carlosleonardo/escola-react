11849c4262976f6befa6d7a1ce5e994b75d2e932
Carlos Leonardo Goes Farias <carlosleonardogoes@outlook.com>
2024-08-22 21:06:08 -0300

    Criados os testes para manipular alunos

--- src/servicos/alunos.servico.ts
+++ src/servicos/alunos.servico.ts
@@ -14,4 +14,12 @@ export class AlunosServico {
   async adicionarAluno(aluno: Aluno): Promise<Aluno> {
     return (await this.conexao.post<Aluno>('/', aluno)).data;
   }
+
+  async removerAluno(id: number): Promise<Aluno> {
+    return (await this.conexao.delete<Aluno>(`/${id}`)).data;
+  }
+
+  async obterAlunoPorId(id: number): Promise<Aluno> {
+    return (await this.conexao.get<Aluno>(`/${id}`)).data;
+  }
 }
